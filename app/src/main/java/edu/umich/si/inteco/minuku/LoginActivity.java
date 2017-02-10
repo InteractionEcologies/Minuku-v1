f34e587cb6d3b447f79ddf383c504e7b602aa20d
Michael Ho <tsungwei50521@gmail.com>
2017-02-10 15:52:19 -0500

    add all user icons

--- app/src/main/java/edu/umich/si/inteco/minuku/LoginActivity.java
+++ app/src/main/java/edu/umich/si/inteco/minuku/LoginActivity.java
@@ -1,8 +1,29 @@
 package edu.umich.si.inteco.minuku;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Spinner;
+
 /**
  * Created by tsung on 2017/2/6.
  */
 
-public class LoginActivity {
+public class LoginActivity extends Activity{
+
+    private Spinner spNumOfUsers;
+//    try to use floating action button
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        init();
+    }
+
+    private void init(){
+        btnLogin = (Button) findViewById(R.id.activity_login_btnLogin);
+        spNumOfUsers = (Spinner) findViewById(R.id.activity_login_sp);
+    }
 }
